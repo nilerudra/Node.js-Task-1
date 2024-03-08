f8aacfacd0a860c5b2a372ea4ec7d059f351b17d
Rudra <rudrakshnile930@gmail.com>
2024-03-08 16:19:54 +0530

    done

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ app.post("/users/forgot-password", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
-});
+module.exports = app;
+
+// app.listen(3000, () => {
+//   console.log("Server is listening on port 3000");
+// });
